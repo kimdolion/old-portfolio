6bb6eee0b1fe330f961e13d7c4a63d7b2a8620cb
kimdolion <kimowilkes@gmail.com>
2019-09-23 00:01:38 -0400

    Reorder tabs and style skills
    - Removed old font-awesome icons in skills
    - Add dev-icons with responsive sizing
    - Add JS to close nav menu after clicking in smaller screens
    - Still need to fix navbar toggler button for smaller screens

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,5 +2,13 @@ $(function () {
   $('.popover-event').popover({trigger: 'hover'})
 })
 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$('.nav-link').on('click', function () {
+  $('.navbar-collapse').collapse('hide')
+})
+
 $(() => {
 })
